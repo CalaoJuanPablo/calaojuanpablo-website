4a9388534bba5328cda04f61c326ddf1bf3f969c
Juan Pablo Calao <jcalaop@gmail.com>
2022-07-19 12:12:50 -0500

    --amend

--- src/components/head/head.tsx
+++ src/components/head/head.tsx
@@ -9,6 +9,7 @@ export const Head: React.FC<HeadProps> = ({ title, description }) => (
   <NextHead>
     <title>{title}</title>
     <meta name='description' content={description} />
+    <meta name='viewport' content='width=device-width, initial-scale=1' />
     <link rel='icon' href='/calaojuanpablo-logo-full-light-bg.svg' />
   </NextHead>
 )
